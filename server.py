71bf3dddff43ea3c46eebe7fb4774d71a696090f
Ashish Gupta <121349721+Ashishgupta291@users.noreply.github.com>
2025-08-20 13:29:49 +0530

    Update server.py

--- server.py
+++ server.py
@@ -386,7 +386,13 @@ if st.session_state["user"] is None:
             auth_url, state = get_auth_url_and_state()
             cookies["oauth_flow"] = "1"
             cookies.save()
-            st.write(f'<meta http-equiv="refresh" content="0; url={auth_url}">', unsafe_allow_html=True)
+            # st.write(f'<meta http-equiv="refresh" content="0; url={auth_url}">', unsafe_allow_html=True)
+            st.write(f"""
+    <script>
+        window.location.href = "{auth_url}";
+    </script>
+""", unsafe_allow_html=True)
+
         except Exception as e:
             print(e)
             st.sidebar.error("Google OAuth setup missing or invalid. Check secrets.")
@@ -490,5 +496,6 @@ if st.session_state["user"] is not None:
 else:
     st.title("Kshitij Chatbot")
     st.write("Please sign up or log in to start chatting.")
+
 
 
